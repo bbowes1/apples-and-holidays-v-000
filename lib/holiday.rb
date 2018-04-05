b10531b215bb0b0e5c8814e0738985b6eb178a02
Benjamin Bowes <bbowes999@gmail.com>
2018-04-05 08:45:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def second_supply_for_fourth_of_july(holiday_hash)
-  # given that holiday_hash looks like this:
+ # given that holiday_hash looks like this:
   # {
   #   :winter => {
   #     :christmas => ["Lights", "Wreath"],
@@ -18,31 +18,56 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+holiday_hash.each do |season, event|
+  event.each do |attribute, data|
+    data.push "Balloons"
+  end
+ end
 end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+holiday_hash.each do |season, event|
+  event.each do |attribute, data|
+    data.push supply
+  end 
+ end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
 
+holiday_hash[season][holiday_name] = supply_array
+ holiday_hash
+ 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter = []
+holiday_hash.each do |season, event|
+ event.each do |attribute, data|
+ 
+ if attribute == :christmas ||  attribute == :new_years
+   data.each do |supply|
+     winter << supply
+  
+   end
+   
+  end
+ end
+ 
+end
+winter
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -54,6 +79,14 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
+  i = 0 
+  supplies = []
+  while i < holiday_hash.length
+  
+  i += i
+
+end
+supplies
 end
 
 def all_holidays_with_bbq(holiday_hash)
